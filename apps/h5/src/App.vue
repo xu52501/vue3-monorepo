87704532f819103cf70a49d46324e7ddc82af838
lx <1154354121@qq.com>
2026-03-19 17:55:58 +0800

    fix:2

--- apps/h5/src/App.vue
+++ apps/h5/src/App.vue
@@ -7,7 +7,7 @@ const a = ref('test');
 <template>
     <div class="text-center">
         <my-button type="primary" shape="round">{{ a }}</my-button>
-        <my-text text="你好"></my-text>
+        <my-text text="文本测试"></my-text>
         <div class="text-4xl text-red-500">github actions 测试</div>
     </div>
 </template>
